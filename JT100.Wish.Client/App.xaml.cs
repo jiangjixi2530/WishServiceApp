3283b13fdc46cd1f6c62cf21242e404f58c71e1f
吉西 姜 <jiangjixi2530@163.com>
2021-06-06 16:37:30 +0800

    更新程序添加

--- JT100.Wish.Client/App.xaml.cs
+++ JT100.Wish.Client/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -16,10 +18,28 @@ namespace JT100.Wish.Client
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            //System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("cmd.exe");
-            //info.FileName = @"c:\Login.bat";
-            //System.Diagnostics.Process proc = System.Diagnostics.Process.Start(info);
-            //proc.WaitForExit();
+            string strPath = AppDomain.CurrentDomain.BaseDirectory + "\\CheckUp.bat";
+            Process process = new Process();//创建进程对象  
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = strPath;//设定需要执行的命令  
+            startInfo.Arguments = "\"" + AppDomain.CurrentDomain.BaseDirectory + "\"";//“/C”表示执行完命令后马上退出  
+            startInfo.UseShellExecute = false;//不使用系统外壳程序启动  
+            startInfo.RedirectStandardInput = false;//不重定向输入  
+            startInfo.RedirectStandardOutput = true; //重定向输出  
+            startInfo.CreateNoWindow = true;//不创建窗口  
+            process.StartInfo = startInfo;
+            try
+            {
+                process.Start();
+            }
+            catch
+            {
+            }
+            Thread.Sleep(1000);
+            while (Process.GetProcessesByName("SimpleUpdater").Length > 0)
+            {
+                Thread.Sleep(1000);
+            }
         }
     }
 }
--- 工具/CheckUp.bat
+++ 工具/CheckUp.bat
@@ -2,6 +2,6 @@
 
 @ECHO OFF
 cd /d %~dp0
-start  %1 SimpleUpdater.exe /startupdate /cv "1.0" /url "http://192.168.1.100:8091/ERPInit/{0}" /infofile "update_c.xml" /autokill /forceupdate /p "UP4.exe
+start  %1 SimpleUpdater.exe /startupdate /cv "1.0" /url "http://www.jizhuhotel.com/download/tool/{0}" /infofile "update_c.xml" /autokill /forceupdate /p "JT100.Wish.Client.exe
 
 exit
